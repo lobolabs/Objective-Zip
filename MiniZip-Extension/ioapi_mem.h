9f73acc8defc34422202e9b5b0b95131a02edff7
Fa[n]tastik <dmitry.koryakin@gmail.com>
2016-02-18 08:55:59 +0200

    Add in memory ioapi

---
+++ MiniZip-Extension/ioapi_mem.h
@@ -0,0 +1,11 @@
+//
+//  ioapi_mem.h
+//  Objective-Zip v. 1.0.2
+//
+//  Created by Dmitry Fantastik on 2/17/16.
+//  Copyright © 2016 Dmitry Fantastik. All rights reserved.
+//
+
+#include "zlib.h"
+
+void fill_memory_filefunc64_32 (zlib_filefunc64_32_def* pzlib_filefunc_def);
